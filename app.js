501454446b134eda4b71040163219744d436fc8e
ramenjuice <ramenjuice@users.noreply.github.com>
2015-10-10 14:38:27 -0700

    working on Passportjs

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/secretlogin', login);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
