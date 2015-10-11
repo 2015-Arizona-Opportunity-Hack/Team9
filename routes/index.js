b37606c3e74d6179785c0a9d36b6121ac944d769
Caleb Frieze <calebfrieze@gmail.com>
2015-10-10 22:57:18 -0700

    worked on twitter api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-var express = require('express');
+var express = require('express'),
+    https = require('https');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // var options = {
+  //   hostname: 'api.twitter.com',
+  //   port: 443,
+  //   path: '/oauth2/token',
+  //   method: 'POST'
+  // };
+  // var req = https.request(options, function(res) {
+  //   console.log("statusCode: ", res.statusCode);
+  //   console.log("headers: ", res.headers);
+  //
+  //   res.on('data', function(d) {
+  //     process.stdout.write(d);
+  //   });
+  // });
   res.render('home', { title: 'Express' });
 });
 
